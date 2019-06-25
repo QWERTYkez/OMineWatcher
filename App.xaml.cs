917fda97a83f37d8bdec75b79e2bcf9018e53aa6
Fantom TM <QWERTYkez@outlook.com>
2019-06-25 18:54:16 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OMineGuard
+{
+    public partial class App : Application
+    {
+
+    }
+
+
+}
