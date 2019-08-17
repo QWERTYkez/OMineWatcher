1a0d946d6deda00efc4038744c60fe095cd1f0c1
Fantom TM <QWERTYkez@outlook.com>
2019-08-17 07:15:37 +0700

    первая страница

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,322 +14,53 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 using System.Net;
-using PM = OMineWatcher.ProfileManager;
 
 namespace OMineWatcher
 {
     public partial class MainWindow : Window
     {
-        static SynchronizationContext MainContext = SynchronizationContext.Current;
+        public static SynchronizationContext MainContext = SynchronizationContext.Current;
+        public static MainWindow ST;
 
         public MainWindow()
         {
             InitializeComponent();
-            BASE = BaseGrid;
-            DETAL = DetalGrid;
-            POOLSSET = PoolSet;
-            BASESET = BaseSet;
-
-            RigGPUs.ItemsSource = new List<String> {"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
-            RigMiner.ItemsSource = new List<Miners> { Miners.Bminer, Miners.Claymore, Miners.GMiner };
-
-            PM.Initialize();
-            RigsList.ItemsSource = PM.Profile.WorkersList.Select(W => W.Name);
-            RigsList_SelectionChanged(null, null);
+            ST = this;
+            StartingApplication();
         }
 
-        #region BASE
-        static Grid BASE;
-
-        #endregion
-        #region DETAL
-        static Grid DETAL;
-
-        #endregion
-        #region RIGSSET
-
-        private void RigsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void StartingApplication()
         {
-            if (RigsList.SelectedIndex != -1)
-            {
-                RigName.IsEnabled = true;
-                RigIP.IsEnabled = true;
-                RigPort.IsEnabled = true;
-                RigMiner.IsEnabled = true;
-                RigGPUs.IsEnabled = true;
-                RigHashrate.IsEnabled = true;
-                RigSave.IsEnabled = true;
-                RigName.Text = PM.Profile.WorkersList[RigsList.SelectedIndex].Name;
-                if (PM.Profile.WorkersList[RigsList.SelectedIndex].IP != null)
-                {
-                    RigIP.Text = PM.Profile.WorkersList[RigsList.SelectedIndex].IP;
-                }
-                else
-                {
-                    RigIP.Text = "";
-                }
-                if (PM.Profile.WorkersList[RigsList.SelectedIndex].Port != null)
-                {
-                    RigPort.Text = PM.Profile.WorkersList[RigsList.SelectedIndex].Port.ToString();
-                }
-                else
-                {
-                    RigPort.Text = "";
-                }
-                if (PM.Profile.WorkersList[RigsList.SelectedIndex].GPUs == null)
-                {
-                    RigGPUs.SelectedIndex = 0;
-                }
-                else
-                {
-                    RigGPUs.SelectedItem = PM.Profile.WorkersList[RigsList.SelectedIndex].GPUs.ToString();
-                }
-                if (PM.Profile.WorkersList[RigsList.SelectedIndex].Miner != null)
-                {
-                    RigMiner.SelectedItem = PM.Profile.WorkersList[RigsList.SelectedIndex].Miner;
-                }
-                else
-                {
-                    RigMiner.SelectedIndex = -1;
-                }
+            GPUsCB.ItemsSource = new string[] 
+            { "Auto", "1", "2", "3", "4", "5", "6", "7", "8",
+                "9", "10", "11", "12", "13", "14", "15", "16" };
+        }
+        private void GPUsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GPUswitchSP.Children.Clear();
 
-                if (PM.Profile.WorkersList[RigsList.SelectedIndex].Hashrate != null)
-                {
-                    RigHashrate.Text = PM.Profile.WorkersList[RigsList.SelectedIndex].Hashrate.ToString();
-                }
-                else
-                {
-                    RigHashrate.Text = "";
-                }
+            string Selected = (string)GPUsCB.SelectedItem;
+            if (Selected == "Auto")
+            {
+                GPUsSwitchHeader.Visibility = Visibility.Hidden;
+                GPUswitchB.Visibility = Visibility.Hidden;
             }
             else
             {
-                RigName.Text = "";
-                RigName.IsEnabled = false;
-                RigIP.Text = "";
-                RigIP.IsEnabled = false;
-                RigPort.Text = "";
-                RigPort.IsEnabled = false;
-                RigMiner.SelectedIndex = -1;
-                RigMiner.IsEnabled = false;
-                RigGPUs.SelectedIndex = -1;
-                RigGPUs.IsEnabled = false;
-                RigHashrate.Text = "";
-                RigHashrate.IsEnabled = false;
-                RigSave.IsEnabled = false;
-            }
-            RigMiner_SelectionChanged(null, null);
-            WachingSwitch();
-        }
-        private void PlusRig_Click(object sender, RoutedEventArgs e)
-        {
-            Worker Wr = new Worker();
-            Wr.Name = "Новый воркер";
-            PM.Profile.WorkersList.Add(Wr);
-            RigsList.ItemsSource = PM.Profile.WorkersList.Select(W => W.Name);
-            RigsList.SelectedIndex = PM.Profile.WorkersList.Count - 1;
-        }
-        private void MinusRig_Click(object sender, RoutedEventArgs e)
-        {
-            PM.Profile.WorkersList.Remove(PM.Profile.WorkersList[RigsList.SelectedIndex]);
-            RigsList.ItemsSource = PM.Profile.WorkersList.Select(W => W.Name);
-        }
-        private void RigSave_Click(object sender, RoutedEventArgs e)
-        {
-            int ind = RigsList.SelectedIndex;
-            int err = 0;
-            double? hashrate = null;
-            if (RigHashrate.Text != "")
-            {
-                try
-                { hashrate = Convert.ToDouble(RigHashrate.Text); }
-                catch (FormatException)
-                { err = 5; }
-                catch (OverflowException)
-                { err = 6; }
-            }
-            Miners? min = null;
-            if (RigMiner.SelectedIndex != -1)
-            {
-                min = (Miners)RigMiner.SelectedItem;
-            }
-            else { err = 7; }
-
-
-            int? port = null;
-            try
-            { port = Convert.ToInt32(RigPort.Text); }
-            catch (FormatException)
-            { err = 3; }
-            catch (OverflowException)
-            { err = 4; }
-            byte? GPUs = null;
-            if ((string)(RigGPUs.SelectedItem) != "")
-            {
-                GPUs = Convert.ToByte((string)(RigGPUs.SelectedItem));
-            }
-            IPAddress ip = null;
-            try
-            { ip = IPAddress.Parse(RigIP.Text); }
-            catch (ArgumentNullException)
-            { err = 1; }
-            catch (FormatException)
-            { err = 2; }
-            
-            if (RigName.Text == "")
-            {
-                RigSendLog("Нужно ввести имя", Brushes.Red);
-            }
-            else if (err == 1)
-            {
-                RigSendLog("Нужно ввести IP", Brushes.Red);
-            }
-            else if (err == 2)
-            {
-                RigSendLog("Неправильный формат IP", Brushes.Red);
-            }
-            else if (err == 3)
-            {
-                RigSendLog("Неправильный формат порта", Brushes.Red);
-            }
-            else if (err == 4)
-            {
-                RigSendLog("Слишком большое значение порта", Brushes.Red);
-            }
-            else if (err == 5)
-            {
-                RigSendLog("Неправильный формат хешрейта", Brushes.Red);
-            }
-            else if (err == 6)
-            {
-                RigSendLog("Слишком большое значение хешрейта", Brushes.Red);
-            }
-            else if (err == 7)
-            {
-                RigSendLog("Выберите майнер", Brushes.Red);
-            }
-            else
-            {
-                Worker Wr = PM.Profile.WorkersList[ind];
-
-                Wr.Name = RigName.Text;
-                Wr.IP = ip.ToString();
-                Wr.Port = port;
-                Wr.GPUs = GPUs;
-                Wr.Miner = min;
-                Wr.Hashrate = hashrate;
-
-                PM.Profile.WorkersList[ind] = Wr;
-
-                RigsList.ItemsSource = PM.Profile.WorkersList.Select(W => W.Name);
-                RigsList.SelectedIndex = ind;
-
-                PM.SaveProfile();
-                RigSendLog("Успешно Сохранено", Brushes.Lime);
-                WachingSwitch();
-            }
-        }
-        #region SendLog
-        private void RigSendLog(string message, Brush brush)
-        {
-            RigLog.Text = message;
-            RigLog.Foreground = brush;
-
-            if (ClearRigLogTHR.IsAlive)
-            {
-                ClearRigLogTHR.Abort();
-            }
-            ClearRigLogTHR = new Thread(() =>
-            {
-                Thread.Sleep(2000);
-                MainContext.Send(ClearRigLog, RigLog);
-            });
-            ClearRigLogTHR.Start();
-        }
-        Thread ClearRigLogTHR = new Thread(() => { });
-        public static void ClearRigLog(object o)
-        {
-            ((TextBlock)o).Text = "";
-        }
-        #endregion
-        private void RigMiner_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (RigMiner.SelectedIndex != -1)
-            {
-                RigHashType.Text = Worker.HashrateTypes[(Miners)RigMiner.SelectedItem];
-            }
-            else
-            {
-                RigHashrate.IsEnabled = false;
-                RigHashType.Text = "";
-            }
-        }
-        private void WachingSwitch()
-        {
-            if(RigsList.SelectedIndex != -1)
-            {
-                bool b1 = PM.Profile.WorkersList[RigsList.SelectedIndex].IP != null;
-                bool b2 = PM.Profile.WorkersList[RigsList.SelectedIndex].Port != null;
-                bool b3 = PM.Profile.WorkersList[RigsList.SelectedIndex].Miner != null;
-                if (b1 & b2 & b3)
+                GPUsSwitchHeader.Visibility = Visibility.Visible;
+                GPUswitchB.Visibility = Visibility.Visible;
+                byte k = Convert.ToByte(Selected);
+                
+                for (byte n = 0; n < k; n++)
                 {
-                    RigWatching.IsEnabled = true;
-                    if (PM.Profile.WorkersList[RigsList.SelectedIndex].Waching == null ||
-                        PM.Profile.WorkersList[RigsList.SelectedIndex].Waching == false)
-                    {
-                        RigWatching.Background = Brushes.Crimson;
-                    }
-                    else
-                    {
-                        RigWatching.Background = Brushes.Lime;
-                    }
-
-                }
-                else
-                {
-                    RigWatching.IsEnabled = false;
+                    Grid GR = new Grid { Width = 60 };
+                    GR.Children.Add(new TextBlock { Text = "GPU" + n, Effect = null });
+                    GR.Children.Add(new CheckBox { Name = "g" + n.ToString(),
+                        Margin = new Thickness(0, 0, 7, 0),
+                        HorizontalAlignment = HorizontalAlignment.Right });
+                    GPUswitchSP.Children.Add(GR);
                 }
             }
-            else
-            {
-                RigWatching.IsEnabled = false;
-            }
         }
-        private void RigWatching_Click(object sender, RoutedEventArgs e)
-        {
-            if (PM.Profile.WorkersList[RigsList.SelectedIndex].Waching == true)
-            {
-                RigWatching.Background = Brushes.Crimson;
-
-                Worker Wr = PM.Profile.WorkersList[RigsList.SelectedIndex];
-                Wr.Waching = false;
-                PM.Profile.WorkersList[RigsList.SelectedIndex] = Wr;
-            }
-            else
-            {
-                RigWatching.Background = Brushes.Lime;
-                Worker Wr = PM.Profile.WorkersList[RigsList.SelectedIndex];
-                Wr.Waching = true;
-                PM.Profile.WorkersList[RigsList.SelectedIndex] = Wr;
-            }
-            PM.SaveProfile();
-        }
-
-        #endregion
-        #region POOLSSET
-        static Grid POOLSSET;
-
-        #endregion
-        #region BASESET
-        static Grid BASESET;
-
-
-
-
-
-
-        #endregion
-
-        
     }
 }
---
+++ Managers/Communication.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OMineGuard.Managers
+{
+    class Communication
+    {
+
+
+        static void GetInfoStream()
+        {
+            TcpClient client;
+            try
+            {
+                using (client = new TcpClient("127.0.0.1", 2112))
+                {
+                    int MessageLength = 15;
+                    string header;
+                    byte[] msg;
+                    string body;
+                    MinerInfo MI;
+
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        Console.WriteLine("старт цикла");
+                        while (true)
+                        {
+                            msg = new byte[MessageLength];     // готовим место для принятия сообщения
+                            int count = stream.Read(msg, 0, msg.Length);   // читаем сообщение от клиента
+                            string request = Encoding.Default.GetString(msg, 0, count);
+                            if (15 == request.Length)
+                            {
+                                continue;
+                            }
+                            string[] req = null;
+                            req = JsonConvert.DeserializeObject<string[]>(request);
+                            header = req[0];
+                            body = req[1];
+
+                            switch (header)
+                            {
+                                case "js":
+                                    {
+                                        MessageLength = Convert.ToInt32(body);
+                                    }
+                                    break;
+                                case "info":
+                                    {
+                                        MI = JsonConvert.DeserializeObject<MinerInfo>(body);
+
+                                        Console.WriteLine($"{MI.TimeStamp.ToShortTimeString()} | {MI.AVGHashrates[0]} | " +
+                                            $"{MI.AVGTemperatures[0]} | {MI.AVGFanspeeds[0]} | {MI.ShAccepted[0]}");
+                                        MessageLength = 15;
+                                    }
+                                    break;
+                            }
+                            Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        public class MinerInfo
+        {
+            public DateTime TimeStamp;
+            public double[] AVGHashrates;
+            public double[] AVGTemperatures;
+            public double[] AVGFanspeeds;
+            public int[] ShAccepted;
+            public int[] ShRejected;
+            public int[] ShInvalid;
+        }
+    }
+}
