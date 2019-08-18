258dc9d2674b73b18f1c90c7eff75a6d6919bbfd
Fantom TM <QWERTYkez@outlook.com>
2019-08-19 06:03:13 +0700

    Группирование ресурсов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,9 +31,8 @@ namespace OMineWatcher
 
         private void StartingApplication()
         {
-            GPUsCB.ItemsSource = new string[] 
-            { "Auto", "1", "2", "3", "4", "5", "6", "7", "8",
-                "9", "10", "11", "12", "13", "14", "15", "16" };
+            GPUsCB.ItemsSource = new string[] { "Auto", "1", "2", "3", "4", "5",
+                "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
         }
         private void GPUsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
