17638815eb807076fbed6e6c4b16082313db152c
Fantom TM <QWERTYkez@outlook.com>
2019-10-25 02:42:34 +0700

    WOL

---
+++ OMineWOL/WOL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMineWOL
+{
+    public static class WOL
+    {
+        public static string GetMACfromIP(string IP)
+        {
+            byte[] ab = new byte[6]; int len = ab.Length;
+            if (SendARP((int)IPAddress.Parse(IP).Address, 0, ab, ref len) == 0)
+            {
+                return BitConverter.ToString(ab);
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        public static extern int SendARP(int DestIP, int SrcIP, [Out] byte[] pMacAddr, ref int PhyAddrLen);
+    }
+}
---
+++ OMineWOLTests/WOLTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OMineWOL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMineWOL.Tests
+{
+    [TestClass()]
+    public class WOLTests
+    {
+        [TestMethod()]
+        public void GetMACfromIPTest()
+        {
+            string ip = "192.168.88.11";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.12";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.113";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.14";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.15";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.21";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+            ip = "192.168.88.31";
+            Debug.WriteLine($"{ip} - {WOL.GetMACfromIP(ip)}");
+        }
+    }
+}
